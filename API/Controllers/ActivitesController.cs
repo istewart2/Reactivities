659ec4de5bc242b9b5041a2b0e2f416d665497ce
istewart2 <iain.stewart@kpmg.co.uk>
2023-07-28 16:57:03 +0100

    Update Get Activity endpoint to use mediatr

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -1,6 +1,5 @@
 using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -18,7 +17,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query { Id = id});
         }
     }
 }
