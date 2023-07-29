43c7e52c418ff909e5d8d0a5d89f789b270424f4
istewart2 <iain.stewart@kpmg.co.uk>
2023-07-29 18:10:46 +0100

    Add endpoint for creating an activity

--- API/Controllers/ActivitesController.cs
+++ API/Controllers/ActivitesController.cs
@@ -19,5 +19,12 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Id = id});
         }
+
+        // api/activities
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
     }
 }
