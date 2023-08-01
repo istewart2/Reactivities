9d502890ecbe3b09327035e3dd029a4ef7975eae
Iain Stewart <iainstewart08@gmail.com>
2023-08-01 22:58:49 +0100

    Add NavBar + images

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then((response) => {
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>('http://localhost:5000/api/activities')
+      .then((response) => {
+        setActivities(response.data);
+      });
   }, []);
 
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities'></Header>
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-    </div>
+    <>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map((activity) => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </>
   );
 }
 
